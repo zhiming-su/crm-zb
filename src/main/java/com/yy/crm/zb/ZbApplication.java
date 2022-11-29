72637884f39eb51dd0fb3232878e11d9932893bc
zhiming-su <sudo_ming@163.com>
2022-11-29 16:51:19 +0800

    add

---
+++ src/main/java/com/yy/crm/zb/ZbApplication.java
@@ -0,0 +1,13 @@
+package com.yy.crm.zb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yy/crm/zb/ZbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yy.crm.zb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
